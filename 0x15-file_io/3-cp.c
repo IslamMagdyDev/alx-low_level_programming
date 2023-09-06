fc2d519079d4e3452f6bc3638834cf4d10917adf
Islam Magdy <136137718+IslamMagdyDev1@users.noreply.github.com>
2023-09-06 03:48:04 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,16 +11,16 @@
  */
 void handleFileErrors(int sourceFileDescriptor, int destinationFileDescriptor, char *arguments[])
 {
-    if (sourceFileDescriptor == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arguments[1]);
-        exit(98);
-    }
-    if (destinationFileDescriptor == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arguments[2]);
-        exit(99);
-    }
+	if (sourceFileDescriptor == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arguments[1]);
+		exit(98);
+	}
+	if (destinationFileDescriptor == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arguments[2]);
+		exit(99);
+	}
 }
 
 /**
@@ -31,44 +31,44 @@ void handleFileErrors(int sourceFileDescriptor, int destinationFileDescriptor, c
  */
 int main(int argc, char *argv[])
 {
-    int sourceFileDescriptor, destinationFileDescriptor, closeStatus;
-    ssize_t bytesRead, bytesWritten;
-    char buffer[1024];
+	int sourceFileDescriptor, destinationFileDescriptor, closeStatus;
+	ssize_t bytesRead, bytesWritten;
+	char buffer[1024];
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: file_copy source_file dest_file\n");
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: file_copy source_file dest_file\n");
+		exit(97);
+	}
 
-    sourceFileDescriptor = open(argv[1], O_RDONLY);
-    destinationFileDescriptor = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-    handleFileErrors(sourceFileDescriptor, destinationFileDescriptor, argv);
+	sourceFileDescriptor = open(argv[1], O_RDONLY);
+	destinationFileDescriptor = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	handleFileErrors(sourceFileDescriptor, destinationFileDescriptor, argv);
 
-    bytesRead = 1024;
-    while (bytesRead == 1024)
-    {
-        bytesRead = read(sourceFileDescriptor, buffer, 1024);
-        if (bytesRead == -1)
-            handleFileErrors(-1, 0, argv);
-        bytesWritten = write(destinationFileDescriptor, buffer, bytesRead);
-        if (bytesWritten == -1)
-            handleFileErrors(0, -1, argv);
-    }
+	bytesRead = 1024;
+	while (bytesRead == 1024)
+	{
+		bytesRead = read(sourceFileDescriptor, buffer, 1024);
+		if (bytesRead == -1)
+			handleFileErrors(-1, 0, argv);
+		bytesWritten = write(destinationFileDescriptor, buffer, bytesRead);
+		if (bytesWritten == -1)
+			handleFileErrors(0, -1, argv);
+	}
 
-    closeStatus = close(sourceFileDescriptor);
-    if (closeStatus == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close source file descriptor %d\n", sourceFileDescriptor);
-        exit(100);
-    }
+	closeStatus = close(sourceFileDescriptor);
+	if (closeStatus == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close source file descriptor %d\n", sourceFileDescriptor);
+		exit(100);
+	}
 
-    closeStatus = close(destinationFileDescriptor);
-    if (closeStatus == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close destination file descriptor %d\n", destinationFileDescriptor);
-        exit(100);
-    }
+	closeStatus = close(destinationFileDescriptor);
+	if (closeStatus == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close destination file descriptor %d\n", destinationFileDescriptor);
+		exit(100);
+	}
 
-    return 0;
+	return 0;
 }
